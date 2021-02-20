21632b14c680e0e5e178d8a597cfc6aa07505675
Olga Buzmakova <olga.buzmakova@gmail.com>
2021-02-20 14:42:45 +0300

    add form validation

---
+++ src/validation.js
@@ -0,0 +1,60 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('popup__text_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__text-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__text_type_error');
+    errorElement.classList.remove('popup__text-error_active');
+    errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
+    const buttonElement = formElement.querySelector('.popup__submit-btn');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', function () {
+            checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.form'));
+    formList.forEach((formElement) => {
+
+        formElement.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+};
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    })
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if(hasInvalidInput(inputList)) {
+        buttonElement.classList.add('popup__submit-btn_inactive');
+    } else {
+        buttonElement.classList.remove('popup__submit-btn_inactive');
+    }
+}
+
+enableValidation();
